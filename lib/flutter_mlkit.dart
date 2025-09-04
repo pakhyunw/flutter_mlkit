77c4a2d3214fd92d3df563178ed031eb1255242e
hyunwoo pak <88700361+pakhyunw@users.noreply.github.com>
2025-09-04 15:26:34 +0900

    ..

--- lib/flutter_mlkit.dart
+++ lib/flutter_mlkit.dart
@@ -63,6 +63,7 @@ class FlutterMlkit {
     LanguageScript? languageScript,
   }) {
     return ScalableOCR(
+      boxHeight:boxHeight,
       roiBoxSize: roiBoxSize,
       getScannedText: getScannedText,
       getRawData: getRawData,
